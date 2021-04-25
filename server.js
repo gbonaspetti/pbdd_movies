0fdcff8476e65434f25d934ac6cc03422b1138c3
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2021-04-25 12:57:15 +0200

    add requests for dead actors and movies from a year

--- server.js
+++ server.js
@@ -7,28 +7,45 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
 
-// Root endpoint
-app.get('/', (req, res, next) => {
-    res.json({'message':'Ok'})
-});
+// Get all the actors who have already died
+app.get('/api/actors/dead', (req, res, next) => {
+  var sql = `SELECT *
+    FROM actors
+    WHERE death_year IS NOT NULL`
 
-// Insert here other API endpoints
-app.get("/api/genres", (req, res, next) => {
-  var sql = "select * from genres"
   var params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
-        res.status(400).json({"error":err.message});
+        res.status(400).json({'error':err.message})
         return;
       }
       res.json({
-          "message":"success",
-          "data":rows
+          'message':'success',
+          'data':rows
       })
-    });
-});
+    })
+})
+
+// Get all the movies from a specific year
+app.get('/api/movies/year/:year', (req, res, next) => {
+  var sql = `SELECT *
+    FROM movies
+    WHERE year = ?`
+
+  var params = [req.params.year]
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({'error':err.message})
+        return;
+      }
+      res.json({
+          'message':'success',
+          'data':rows
+      })
+    })
+})
 
 // Default response for any other request
 app.use(function(req, res){
-    res.status(404);
-});
+    res.status(404)
+})
