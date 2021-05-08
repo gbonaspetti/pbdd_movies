dba1331f4d588113e482a56331d498bf83f95d71
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2021-05-08 21:06:05 +0200

    some linting and add multiple actors request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var db = require('./database.js')
+const db = require('./database.js')
 const port = 3000
 
 app.listen(port, () => {
@@ -10,13 +10,13 @@ app.listen(port, () => {
 // Get all the actors who have already died and their age
 // Note: To be able to calculate their age, the birth must be known
 app.get('/api/actors/dead', (req, res, next) => {
-  var sql = `SELECT COUNT(id) as actors, (death_year - birth_year) as age
+  const sql = `SELECT COUNT(id) as actors, (death_year - birth_year) as age
     FROM actors
     WHERE death_year IS NOT NULL
       AND birth_year IS NOT NULL
     GROUP BY age`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -32,7 +32,7 @@ app.get('/api/actors/dead', (req, res, next) => {
 // Get the age average of the actors who have already died
 // Note: To be able to calculate their age, the birth must be known
 app.get('/api/actors/deadAVG', (req, res, next) => {
-  var sql = `SELECT AVG(age) as deathAvg
+  const sql = `SELECT AVG(age) as deathAvg
   FROM (
     SELECT (death_year - birth_year) as age
     FROM actors
@@ -40,7 +40,7 @@ app.get('/api/actors/deadAVG', (req, res, next) => {
       AND birth_year IS NOT NULL
   )`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -56,13 +56,13 @@ app.get('/api/actors/deadAVG', (req, res, next) => {
 // Get all the actors who are still alived group by age
 // Note: To be able to calculate their age, the birth must be known
 app.get('/api/actors/ages', (req, res, next) => {
-  var sql = `SELECT GROUP_CONCAT(name) as names, (2021 - birth_year) as age
+  const sql = `SELECT GROUP_CONCAT(name) as names, (2021 - birth_year) as age
     FROM actors
     WHERE death_year IS NULL
       AND birth_year IS NOT NULL
     GROUP BY birth_year`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -77,12 +77,12 @@ app.get('/api/actors/ages', (req, res, next) => {
 
 // Get all the movies (title, rating, votes) from a specific year
 app.get('/api/movies/year/:year', (req, res, next) => {
-  var sql = `SELECT title, rating, votes
+  const sql = `SELECT title, rating, votes
     FROM movies
     WHERE year = ?
     ORDER BY rating DESC`
 
-  var params = [req.params.year]
+  const params = [req.params.year]
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -97,13 +97,13 @@ app.get('/api/movies/year/:year', (req, res, next) => {
 
 // Get quantity of movies each year
 app.get('/api/movies/year', (req, res, next) => {
-  var sql = `SELECT COUNT(id) as quantity, year
+  const sql = `SELECT COUNT(id) as quantity, year
     FROM movies
     WHERE year IS NOT NULL
     GROUP BY year
     ORDER BY year DESC`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -119,14 +119,14 @@ app.get('/api/movies/year', (req, res, next) => {
 
 // Get the years that produced better rating movies
 app.get('/api/movies/most/year', (req, res, next) => {
-  var sql = `SELECT year, AVG(rating) as rating
+  const sql = `SELECT year, AVG(rating) as rating
     FROM movies
     WHERE rating IS NOT NULL
       AND year IS NOT NULL
     GROUP BY year
     ORDER BY rating DESC`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -141,7 +141,7 @@ app.get('/api/movies/most/year', (req, res, next) => {
 
 // Get the actors that produced better rated movies
 app.get('/api/actors/most/year', (req, res, next) => {
-  var sql = `SELECT name, AVG(rating) as rating, COUNT(movie_id) as movies
+  const sql = `SELECT name, AVG(rating) as rating, COUNT(movie_id) as movies
     FROM actors
     INNER JOIN castings ON castings.actor_id = actors.id
     INNER JOIN movies ON castings.movie_id = movies.id
@@ -149,7 +149,7 @@ app.get('/api/actors/most/year', (req, res, next) => {
     GROUP BY actors.id
     ORDER BY rating DESC`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -164,14 +164,14 @@ app.get('/api/actors/most/year', (req, res, next) => {
 
 // Get actors with that made more movies
 app.get('/api/actors/movies', (req, res, next) => {
-  var sql = `SELECT name, COUNT(movie_id) as quantity, GROUP_CONCAT(title) as titles
+  const sql = `SELECT name, COUNT(movie_id) as quantity, GROUP_CONCAT(title) as titles
     FROM castings
     INNER JOIN movies ON castings.movie_id = movies.id
     INNER JOIN actors ON castings.actor_id = actors.id
     GROUP BY actor_id
     ORDER BY quantity DESC`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -186,14 +186,14 @@ app.get('/api/actors/movies', (req, res, next) => {
 
 // Get all genres with rating average and number of movies
 app.get('/api/genres/rating', (req, res, next) => {
-  var sql = `SELECT name, AVG(rating) as rating
+  const sql = `SELECT name, AVG(rating) as rating
     FROM classifications
     INNER JOIN movies ON movies.id = classifications.movie_id
     INNER JOIN genres ON genres.id = classifications.genre_id
     GROUP BY genre_id
     ORDER BY rating DESC`
 
-  var params = []
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
@@ -208,14 +208,40 @@ app.get('/api/genres/rating', (req, res, next) => {
 
 // Get all genres with rating average and number of movies
 app.get('/api/genres/count', (req, res, next) => {
-  var sql = `SELECT name, COUNT(movie_id) as quantity
+  const sql = `SELECT name, COUNT(movie_id) as quantity
     FROM classifications
     INNER JOIN movies ON movies.id = classifications.movie_id
     INNER JOIN genres ON genres.id = classifications.genre_id
     GROUP BY genre_id
     ORDER BY quantity DESC`
 
-  var params = []
+  const params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({'error':err.message})
+        return
+      }
+      res.status(200).json({
+          'message':'success',
+          'data':rows
+      })
+    })
+})
+
+// Get movies made by 2 or more actors
+app.get('/api/actors/multiple', (req, res, next) => {
+  const sql = `SELECT title, actorsNames
+    FROM (
+      SELECT title, COUNT(name) as actorsNumber, GROUP_CONCAT(name) as actorsNames
+      FROM movies
+      INNER JOIN castings ON castings.movie_id = movies.id
+      INNER JOIN actors ON actors.id = castings.actor_id
+      WHERE name IN (${req.body.actorList})
+      GROUP BY movies.id
+    )
+    WHERE actorsNumber > 1`
+
+  const params = []
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({'error':err.message})
