270ae7d3d151d19015fbf22beeb353b03e5101a8
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2021-05-13 12:08:26 +0200

    update list categories

--- pbdd_movies/src/RequestOptions.jsx
+++ pbdd_movies/src/RequestOptions.jsx
@@ -6,55 +6,67 @@ import {
 import { PAGE } from './helper.js'
 
 class RequestOptions extends React.Component {
-  render () {
+  render() {
     return (
       <div>
-        Funcionalidades
+        <h2>Funcionalidades</h2>
         <ul>
-          <li>
-            <Link to={PAGE.ACTORS.AGE}>
-              Idade dos atores
-            </Link>
+          <li>Sobre atores:
+            <ul>
+              <li>
+                <Link to={PAGE.ACTORS.AGE}>
+                  Idade dos atores
+                </Link>
+              </li>
+              <li>
+                <Link to={PAGE.ACTORS.LIFE_EXPECTANCY}>
+                  Expectativa de vida de atores
+                </Link>
+              </li>
+              <li>
+                <Link to={PAGE.ACTORS.RATED}>
+                  Atores que produziram os filmes mais bem classificados
+                </Link>
+              </li>
+              <li>
+                <Link to={PAGE.ACTORS.MOVIES}>
+                  Atores que participaram de mais filmes
+                </Link>
+              </li>
+            </ul>
           </li>
-          <li>
-            <Link to={PAGE.MOVIES.YEAR}>
-              Filmes produzidos em um ano especifico
-            </Link>
+          <li>Sobre filmes:
+            <ul>
+              <li>
+                <Link to={PAGE.MOVIES.YEAR}>
+                  Filmes produzidos em um ano específico
+                </Link>
+              </li>
+              <li>
+                <Link to={PAGE.MOVIES.QUANTITY_YEAR}>
+                  A quantidade de filmes produzida em cada ano
+                </Link>
+              </li>
+              <li>
+                <Link to={PAGE.MOVIES.BEST_YEAR}>
+                  Anos que produziram os filmes mais bem classificados
+                </Link>
+              </li>
+            </ul>
           </li>
-          <li>
-            <Link to={PAGE.MOVIES.QUANTITY_YEAR}>
-              A quantidade de filmes produzida em cada ano
-            </Link>
-          </li>
-          <li>
-            <Link to={PAGE.MOVIES.BEST_YEAR}>
-              Anos que produziram mais filmes de “qualidades” (notas mais altas no IMDb)
-            </Link>
-          </li>
-          <li>
-            <Link to={PAGE.ACTORS.LIFE_EXPECTANCY}>
-              Expectativa de vida de atores
-            </Link>
-          </li>
-          <li>
-            <Link to={PAGE.GENRES.RATED}>
-              Quais são os gêneros mais bem classificados
-            </Link>
-          </li>
-          <li>
-            <Link to={PAGE.ACTORS.RATED}>
-              Atores que produziram mais filmes de “qualidades” (notas mais altas no IMDb)
-            </Link>
-          </li>
-          <li>
-            <Link to={PAGE.ACTORS.MOVIES}>
-              Quais atores fizeram mais filmes
-            </Link>
-          </li>
-          <li>
-            <Link to={PAGE.GENRES.PRODUCED}>
-              Quais são os gêneros mais produzidos
-            </Link>
+          <li>Sobre gêneros:
+            <ul>
+              <li>
+                <Link to={PAGE.GENRES.RATED}>
+                  Quais são os gêneros mais bem classificados
+                </Link>
+              </li>
+              <li>
+                <Link to={PAGE.GENRES.PRODUCED}>
+                  Quais são os gêneros mais produzidos
+                </Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
