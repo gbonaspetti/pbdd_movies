f421ea1431e216dd7778cf41ead0f041d17ce6d2
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2021-05-13 12:09:29 +0200

    update life expectancy display

--- pbdd_movies/src/RequestsResults/ActorsLifeExpectancy.jsx
+++ pbdd_movies/src/RequestsResults/ActorsLifeExpectancy.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import CanvasJSReact from '../assets/canvasjs.react'
 import {
   getActorsLifeExpectancyAverage,
   getActorsLifeExpectancyList
 } from '../async.js'
 import { generateFetchResponse } from '../helper.js'
+
+const CanvasJS = CanvasJSReact.CanvasJS
+const CanvasJSChart = CanvasJSReact.CanvasJSChart
+CanvasJS.addCultureInfo('pt_BR',
+  {
+    decimalSeparator: ',',
+    digitGroupSeparator: '.'
+  })
 
 class ActorsLifeExpectancy extends React.Component {
   constructor(props) {
@@ -24,15 +33,34 @@ class ActorsLifeExpectancy extends React.Component {
     })
   }
 
-  render () {
+  render() {
+    const { state } = this
+    const graphicData = {
+      animationEnabled: true,
+      axisX: {
+        title: "Idade"
+      },
+      axisY: {
+        title: 'Quantidade de atores'
+      },
+      culture: 'pt_BR',
+      data: [{
+        type: "line",
+        toolTipContent: "{y} atores morreram aos {x} anos",
+        dataPoints: state.actorsLifeExpectancyList.map(actorsAge => ({
+          x: actorsAge.age,
+          y: actorsAge.actors
+        }))
+      }],
+      theme: 'light2'
+    }
+
     return (
       <div>
-        <p>Média: {this.state.actorsLifeExpectancyAverage}</p>
-        {this.state.actorsLifeExpectancyList.map(actorsAge =>
-          <p key={actorsAge.age}>
-            {actorsAge.actors} - {actorsAge.age}
-          </p>
-        )}
+        <h2>Expectativa de vida dos atores</h2>
+
+        <p>Média: {state.actorsLifeExpectancyAverage} anos</p>
+        <CanvasJSChart options={graphicData} />
       </div>
     )
   }
